52e18d80c956620224aa17050b886527ba2a43d8
huasuoworld <huasuoworld@outlook.com>
2018-11-22 17:39:24 +0800

    commit

--- src/main/java/com/huasuoworld/fundation/Runner.java
+++ src/main/java/com/huasuoworld/fundation/Runner.java
@@ -3,29 +3,50 @@ package com.huasuoworld.fundation;
 import com.google.inject.servlet.GuiceFilter;
 import com.huasuoworld.fundation.guice.InitializeGuiceModulesContextListener;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 
 import javax.servlet.DispatcherType;
 import java.util.EnumSet;
 
+import javax.xml.bind.JAXBContext;
+
 public class Runner {
 
     private static Server server;
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        new Runner().run();
     }
 
-    private void startServer() throws Exception {
-        server.start();
+    private void run() throws Exception {
+        createServer();
+        bindGuiceContextToServer();
+        startServer();
+        waitForServerToFinnish();
     }
 
-    private void createServer() {
-        server = new Server(10005);
+    private void bindGuiceContextToServer() {
+        ServletContextHandler context = createRootContext();
+        serveGuiceContext(context);
     }
 
-    private void waitForServerToFinnish() throws InterruptedException {
-        server.join();
+    private void serveGuiceContext(ServletContextHandler context) {
+        bindGuiceContextAndFilter(context);
+        addDefaultServletToContext(context);
+    }
+
+    private void addDefaultServletToContext(ServletContextHandler context) {
+        /*
+         * Jetty requires some servlet to be bound to the path, otherwise request is just skipped. This prevents Guice
+         * from handling the request, because it is done through filter.
+         */
+        context.addServlet(DefaultServlet.class, "/");
+    }
+
+    private void bindGuiceContextAndFilter(ServletContextHandler context) {
+        context.addEventListener(new InitializeGuiceModulesContextListener());
+        context.addFilter(GuiceFilter.class, "/*", EnumSet.allOf(DispatcherType.class));
     }
 
     private ServletContextHandler createRootContext() {
@@ -35,8 +56,15 @@ public class Runner {
         return context;
     }
 
-    private void bindGuiceContextAndFilter(ServletContextHandler context) {
-        context.addEventListener(new InitializeGuiceModulesContextListener());
-        context.addFilter(GuiceFilter.class, "/*", EnumSet.allOf(DispatcherType.class));
+    private void waitForServerToFinnish() throws InterruptedException {
+        server.join();
+    }
+
+    private void startServer() throws Exception {
+        server.start();
+    }
+
+    private void createServer() {
+        server = new Server(10010);
     }
 }
