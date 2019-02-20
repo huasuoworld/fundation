9d3403ba54898fda6f06c386069c1ea351b82d32
huasuoworld <huasuoworld@outlook.com>
2019-02-20 18:01:24 +0800

    use remote openapi json

--- src/main/java/www/huasuoworld/com/webapiservice/persistence/TransactionPersistence.java
+++ src/main/java/www/huasuoworld/com/webapiservice/persistence/TransactionPersistence.java
@@ -14,6 +14,7 @@ import java.util.function.Predicate;
  *
  * @author slinkydeveloper
  */
+
 public interface TransactionPersistence {
 
 
--- src/main/java/www/huasuoworld/com/webapiservice/persistence/impl/TransactionPersistenceImpl.java
+++ src/main/java/www/huasuoworld/com/webapiservice/persistence/impl/TransactionPersistenceImpl.java
@@ -107,7 +107,7 @@ public class TransactionPersistenceImpl implements TransactionPersistence {
             });
           });
       });
-    });
+    }).close();
   }
 
   @Override
